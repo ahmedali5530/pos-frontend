be5007093c57664c9879e00bcd64e464aee74911
Ahmed Ali <ahmedali5530@gmail.com>
2026-04-03 13:46:42 +0500

    added category filter in inventory

--- src/app-frontend/components/inventory/inventory/index.tsx
+++ src/app-frontend/components/inventory/inventory/index.tsx
@@ -4,7 +4,7 @@ import {Tables} from "../../../../api/db/tables";
 import {ReactSelect} from "../../../../app-common/components/input/custom.react.select";
 import {Product} from "../../../../api/model/product";
 import {Store} from "../../../../api/model/store";
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useMemo, useState} from "react";
 import {useAtom} from "jotai";
 import {appState} from "../../../../store/jotai";
 import {ProductStore} from "../../../../api/model/product.store";
@@ -12,22 +12,25 @@ import {TableComponent} from "../../../../app-common/components/table/table";
 import {createColumnHelper} from "@tanstack/react-table";
 import {formatNumber} from "../../../../lib/currency/currency";
 import {toRecordId} from "../../../../api/model/common";
+import {Switch} from "../../../../app-common/components/input/switch";
 
 export const InventoryDetails = () => {
   const db = useDB();
   const [{store: CurrentStore}] = useAtom(appState);
 
-  const {data: products} = useApi<SettingsData<Product>>(Tables.product, [], [], 0, undefined, [], {}, ['id', 'name']);
+  const {data: products} = useApi<SettingsData<Product>>(Tables.product, [], [], 0, undefined, ['categories'], {}, ['id', 'name', 'categories']);
   const {data: stores} = useApi<SettingsData<Store>>(Tables.store, [], [], 0, undefined, [], {}, ['id', 'name']);
 
   const useLoadHook = useApi<SettingsData<ProductStore>>(Tables.product_store, [], [], 0, 10, [
     'store', 'variants.store', 'variants.variant', 'product', 'variants.product', 'product.categories'
   ], {}, ['id', 'product.name', 'product.categories', 'product.purchase_unit', 'quantity', 'store.name',
-    '(SELECT product.name, product.purchase_unit, quantity, store.name, variant.attribute_value FROM product_variant_store WHERE product = $parent.product AND store = $parent.store) AS variants'
+    '(SELECT product.name, product.purchase_unit, quantity, re_order_level, store.name, variant.attribute_value FROM product_variant_store WHERE product = $parent.product AND store = $parent.store) AS variants'
   ]);
 
   const [filteredProducts, setFilteredProducts] = useState([]);
+  const [filteredCategory, setFilteredCategory] = useState<{label: string, value: string}|undefined>();
   const [store, setStore] = useState();
+  const [zeroItems, setZeroItems] = useState(false);
 
   useEffect(() => {
     useLoadHook.resetFilters();
@@ -43,7 +46,24 @@ export const InventoryDetails = () => {
         `product.id IN [${filteredProducts.map(item => toRecordId(item.value)).join(',')}]`
       )
     }
-  }, [filteredProducts, store]);
+
+    if(filteredCategory){
+      useLoadHook.addFilter(
+        `product.categories *= ${toRecordId(filteredCategory.value)}`
+      )
+    }
+
+    if(zeroItems){
+      useLoadHook.addFilter(
+        `product_variant_store.quantity <= 0`
+      )
+    }else{
+      useLoadHook.addFilter(
+        `quantity > 0`
+      )
+    }
+
+  }, [filteredProducts, store, filteredCategory, zeroItems]);
 
   const columnHelper = createColumnHelper();
   const columns: any = [
@@ -69,6 +89,12 @@ export const InventoryDetails = () => {
       enableColumnFilter: false,
       cell: info => `${formatNumber(info.getValue())} ${info.row.original.product.purchase_unit}`
     }),
+    columnHelper.accessor('re_order_level', {
+      header: 'Re order level',
+      enableSorting: false,
+      enableColumnFilter: false,
+      cell: info => info.getValue() ? `${formatNumber(info.getValue())} ${info.row.original.product.purchase_unit}` : '-'
+    }),
     columnHelper.accessor('id', {
       header: 'Variants',
       enableSorting: false,
@@ -90,6 +116,21 @@ export const InventoryDetails = () => {
     })
   ];
 
+  const categories = useMemo(() => {
+    const list = new Map();
+
+    products?.data?.forEach(item => {
+      item?.categories?.forEach(c => {
+        list.set(c.id, {
+          label: c.name,
+          value: c.id
+        })
+      })
+    });
+
+    return Array.from(list.values());
+  }, [products?.data]);
+
   return (
     <div>
       <div className="flex flex-row gap-5">
@@ -106,6 +147,21 @@ export const InventoryDetails = () => {
           />
         </div>
         <div className="flex-1">
+          <label htmlFor="categories">Category</label>
+          <ReactSelect
+            options={categories}
+            value={filteredCategory}
+            isClearable
+            onChange={(value) => {
+              if(value) {
+                setFilteredCategory(value);
+              }else {
+                setFilteredCategory(undefined);
+              }
+            }}
+          />
+        </div>
+        <div className="flex-1">
           <label htmlFor="products">Product</label>
           <ReactSelect
             options={products?.data?.map(item => ({
@@ -116,6 +172,15 @@ export const InventoryDetails = () => {
             onChange={setFilteredProducts}
             isMulti
           />
+        </div>
+        <div className="flex-1 self-end">
+          <Switch
+            onChange={(e) => setZeroItems(e.currentTarget.checked)}
+            checked={zeroItems}
+            id="zero-items"
+          >
+            <label htmlFor="zero-items">Zero or less then zero items</label>
+          </Switch>
         </div>
       </div>
 
